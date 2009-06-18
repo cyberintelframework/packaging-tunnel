67da96024b0e547ef895fb6799e26d8d6041fccf
Kees Trippelvitz <github@nabber.nl>
2009-06-18 11:55:15 +0000

    Adding 3.0rc1 package building files.
    git-svn-id: https://svn.surfnet.nl/svn/ids/packaging/tunnel/tags/3.0rc1@1190 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

---
+++ make_release.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+KEYID=0000000
+MAKEROOT=/home/build/tunnel
+DIST=lenny
+REPOSITORY=/opt/surfnetids/repositories/surfids
+CHROOTBUILD=$MAKEROOT/chrootimg/$DIST.tgz
+CHROOTTEST=$MAKEROOT/chrootimg/$DIST_test.tgz
+MIRROR=http://ftp.nl.debian.org/debian
+PACKAGE=surfids-tunnel
+
+# Clean up previous build
+cd $MAKEROOT
+rm ${PACKAGE}_*
+
+# Update or create the chroot environment
+if [ -e $CHROOTBUILD ]
+then
+    sudo pbuilder --update --basetgz $CHROOTBUILD --distribution $DIST --mirror $MIRROR
+else
+    sudo pbuilder --create --basetgz $CHROOTBUILD --distribution $DIST --mirror $MIRROR
+fi
+
+
+## Update to latest version
+# If this fails, please checkout the sensor trunk:
+# cd $MAKEROOT
+#  svn co http://svn.ids.surfnet.nl/surfids/logserver/trunk
+svn export http://svn.ids.surfnet.nl/surfids/tunnel/trunk $MAKEROOT/trunk/
+cd $MAKEROOT/trunk
+cp -R ./* $MAKEROOT/$PACKAGE/opt/surfnetids/
+mv $MAKEROOT/$PACKAGE/opt/surfnetids/dhclient.conf $MAKEROOT/$PACKAGE/etc/surfnetids/
+mv $MAKEROOT/$PACKAGE/opt/surfnetids/surfnetids-tn.conf $MAKEROOT/$PACKAGE/etc/surfnetids/
+mv $MAKEROOT/$PACKAGE/opt/surfnetids/surfnetids-tn-apache.conf $MAKEROOT/$PACKAGE/etc/surfnetids/
+rm -rf $MAKEROOT/trunk
+cd $MAKEROOT/$PACKAGE
+
+# Increment changelog entry
+dch -i -m
+#svn commit
+
+## create the package
+pdebuild -- --basetgz $CHROOTBUILD
+#dpkg-deb --build $PACKAGE
+
+### Test the package
+#if [ -s $CHROOTTEST ]
+#then
+#    sudo piuparts -b  $CHROOTTEST -d $DIST *.deb -m $MIRROR
+#else
+#    sudo piuparts -s  $CHROOTTEST -d $DIST *.deb -m $MIRROR
+#   # check exit code
+#fi
+
+
+# Collect results and sign
+cd $MAKEROOT
+sudo mv /var/cache/pbuilder/result/${PACKAGE}_* .
+debsign -k$KEYID ${PACKAGE}_*_i386.changes
+
+# add package to repository
+cd $REPOSITORY
+sudo reprepro include $DIST $MAKEROOT/$PACKAGE_*_i386.changes
