b110b0845cd15f40f36d50270a792e56755ece26
Kees Trippelvitz <github@nabber.nl>
2011-06-07 10:23:09 +0000

    changed help text for the htpasswd stuff
    git-svn-id: https://svn.surfnet.nl/svn/ids/packaging/tunnel/trunk@1821 6e3b1d27-7b68-4cde-b5d6-ae3f64e5b54f

--- debian/cron.d
+++ debian/cron.d
@@ -1,4 +1,5 @@
 */5 * * * * root /opt/surfnetids/scripts/rrd_traffic.pl >/dev/null
 */5 * * * * root /opt/surfnetids/scripts/rrd_serverinfo.pl >/dev/null
+*/10 * * * * root /opt/surfnetids/scripts/tun-janitor.pl >/dev/null
 59 23 * * * root /opt/surfnetids/scripts/scanbinaries.pl >/dev/null
 #* 5 * * * root /opt/surfnetids/scripts/redirect_argos.pl >/dev/null
